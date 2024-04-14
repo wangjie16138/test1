12f9ee3312fe61d67601331c6bff692e937c7797
wangjie16138 <155615428+wangjie16138@users.noreply.github.com>
2024-04-14 13:42:07 +0800

    Delete Study_File directory

--- Study_File/数据分析/data_define.py
+++
@@ -1,7 +0,0 @@
-
-class Record:
-    def __init__(self,data,order_id,money,province):
-        self.data=data
-        self.order_id=order_id
-        self.money=money
-        self.province=province
